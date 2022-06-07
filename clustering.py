398f1bb83889cf2c01ff5c8bdbc4e2ad9c0193e2
Arefeh Alaee <Arefeh.Alaee@Maat.local>
2022-06-07 11:51:41 +0430

    first commit

---
+++ clustering.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+import arcpy
+
+classes=["A","B"]
+TargetAges=['MAN_BTW_20','MAN_BTW_30']
+brand_name=['brand_name']
+
+df = pd.read_csv (r'D:/atrin/atrin/clustering/TAZ_BlockData_Aggregate.csv')
+#calculating the number of targetAudiences
+df['TA']= df[TargetAges].sum(axis=1)
+print(df)
+#sicialclass filtering
+filt=df['SocialClass'].isin(classes)
+df_TargetSocialClass=df[filt]
+df2=df_TargetSocialClass[['TA','zone97','SocialClass']]
+zones=df['zones']
+TargetAudience=pd.merge(zones,df2,left_on='zones',right_on='zone97',how='outer')
+TargetAudience['TA'] = TargetAudience['TA'].fillna(0)
+TargetAudience.to_csv('D:/atrin/atrin/clustering/TargetAudience.csv')
+
+
+arcpy.env.workspace = "D:/atrin/atrin/clustering"
+TargetAudience=pd.read_csv (r'D:/atrin/atrin/clustering/TargetAudience.csv')
+arcpy.MakeFeatureLayer_management("clustering.gdb/TAZ", "temmp")
+arcpy.AddJoin_management("temmp", "zone97", 'TargetAudience.csv', "zones")
+arcpy.CopyFeatures_management("temmp", "clustering.gdb/TAZ_join")
+TAZ_join='clustering.gdb/TAZ_join'     #filter_final_featureclass
+
+outputt='cluster{0}.shp'.format(brand_name)
+outputt='clustering.gdb/cluster'
+arcpy.stats.ClustersOutliers(TAZ_join, 'TargetAudience_csv_TA', outputt, 'INVERSE_DISTANCE', 'EUCLIDEAN_DISTANCE', 'NONE')
+outputt_xls='cluster.xls'
+arcpy.conversion.TableToExcel(outputt, outputt_csv)
+
+dfC = pd.read_excel('D:/atrin/atrin/clustering/cluster.xls')
+dfTAZ=pd.read_csv('D:/atrin/atrin/clustering/TAZ.csv')
+final=pd.merge(dfC,dfTAZ,left_on='SOURCE_ID',right_on='OBJECTID',how='outer')
+finall=final[['TAZ_zone97','COType','TargetAudience_csv_TA']]
+
+finall.to_csv('D:/atrin/atrin/clustering/final.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+##############################################################################
+# from esda.moran import Moran
+# from libpysal.weights import Queen, KNN
+# import seaborn 
+# import pandas
+# import geopandas 
+# import numpy
+# import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt  # Graphics
+# from matplotlib import colors
+# import seaborn                   # Graphics
+# # import geopandas                 # Spatial data manipulation
+# import pandas                    # Tabular data manipulation
+# import rioxarray                 # Surface data manipulation
+# import xarray                    # Surface data manipulation
+# from pysal.explore import esda   # Exploratory Spatial analytics
+# from pysal.lib import weights    # Spatial weights
+# import contextily                # Background tiles
+
+
+
+# # lisa = esda.moran.Moran_Local(db['Pct_Leave'], w)
+# print ('hello')
+
+# import seaborn as sns
+# import pandas as pd
+# import esda
+# # from pysal.lib import weights
+# # from splot.esda import moran_scatterplot, lisa_cluster, plot_local_autocorrelation
+# # import geopandas as gpd 
+# import numpy as np
+# # import contextily as ctx
+# import matplotlib.pyplot as plt
+
+
+
