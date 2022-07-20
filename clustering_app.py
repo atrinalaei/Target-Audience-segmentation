dbef3d79f7890c082e37f96d7cef429c4a983b76
Arefeh Alaee <Arefeh.Alaee@Maat.local>
2022-07-20 15:47:27 +0430

    desination

--- clustering_app.py
+++ clustering_app.py
@@ -1,19 +1,18 @@
 import numpy as np
 import pandas as pd
 import arcpy
-import timestamp
 from datetime import datetime
 import os
 
 
 ########################################################################################################
 # inputs
-dir='D:/atrin/atrin/clustering'
-classes=["A","B"]
-TargetAges=['WOM_BTW_0_','WOM_BTW_5_','WOM_BTW_10','WOM_BTW_15','WOM_BTW_20','WOM_BTW_25','WOM_BTW_30','WOM_BTW_35','WOM_BTW_40','WOM_BTW_45','WOM_BTW_50','WOM_BTW_55','WOM_BTW_60','WOM_BTW_65','WOM_BTW_70']
-TargetAgess=['WOM_BTW_0_','WOM_BTW_5_','WOM_BTW_10','WOM_BTW_15','WOM_BTW_20','WOM_BTW_25','WOM_BTW_30','WOM_BTW_35','WOM_BTW_40','WOM_BTW_45','WOM_BTW_50','WOM_BTW_55','WOM_BTW_60','WOM_BTW_65','WOM_BTW_70','zone97','SocialClass']
-TargetAgesss=['TA','WOM_BTW_0_','WOM_BTW_5_','WOM_BTW_10','WOM_BTW_15','WOM_BTW_20','WOM_BTW_25','WOM_BTW_30','WOM_BTW_35','WOM_BTW_40','WOM_BTW_45','WOM_BTW_50','WOM_BTW_55','WOM_BTW_60','WOM_BTW_65','WOM_BTW_70','zone97','SocialClass']
-TargetAgessss=['SOURCE_ID','WOM_BTW_0_','WOM_BTW_5_','WOM_BTW_10','WOM_BTW_15','WOM_BTW_20','WOM_BTW_25','WOM_BTW_30','WOM_BTW_35','WOM_BTW_40','WOM_BTW_45','WOM_BTW_50','WOM_BTW_55','WOM_BTW_60','WOM_BTW_65','WOM_BTW_70','zone97','SocialClass']
+dir="D:/atrin/atrin/clustering"
+classes=["A","B","C1"]
+TargetAges=['W0','W5','W10','W15','W20','W25','W30','W35','W40','W45','W50','W55','W60','W65','W70']
+TargetAgess=['W0','W5','W10','W15','W20','W25','W30','W35','W40','W45','W50','W55','W60','W65','W70','zone97','SocialClass','POP','Regions']
+TargetAgesss=['TA','W0','W5','W10','W15','W20','W25','W30','W35','W40','W45','W50','W55','W60','W65','W70','zone97','SocialClass','POP','Regions']
+TargetAgessss=['SOURCE_ID','W0','W5','W10','W15','W20','W25','W30','W35','W40','W45','W50','W55','W60','W65','W70','zone97','SocialClass','POP','Regions']
 brand_name='BrandName'
 #priority
 i=0         # please start from 0 , based on TargetAges list
@@ -23,9 +22,8 @@ jj=j+14
 ##################################################################################################
 now = datetime.now()
 date_time = now.strftime("%Y%m%d_%H%M%S")
-arcpy.env.workspace = "{0}".format(dir)
-
-df = pd.read_csv (r'TAZ_BlockData_Aggregate.csv')
+arcpy.env.workspace = dir
+df = pd.read_csv (r'{0}/TAZ_BlockData_Aggregatee.csv'.format(dir))
 #calculating the number of targetAudiences
 df['TA']= df[TargetAges].sum(axis=1)
 
@@ -38,19 +36,19 @@ df2=df_TargetSocialClass[TargetAgesss]
 zones=df['zones']
 TargetAudience=pd.merge(zones,df2,left_on='zones',right_on='zone97',how='outer')
 TargetAudience['TA'] = TargetAudience['TA'].fillna(0)
-TargetAudience.to_csv('TargetAudience.csv')
+TargetAudience.to_csv('{0}/TAA.csv'.format(dir))
 
 
 
-TargetAudience=pd.read_csv (r'TargetAudience.csv')
+TargetAudience=pd.read_csv (r'{0}/TAA.csv'.format(dir))
 arcpy.MakeFeatureLayer_management("clustering.gdb/TAZ", "temmp")
-arcpy.AddJoin_management("temmp", "zone97", 'TargetAudience.csv', "zones")
+arcpy.AddJoin_management("temmp", "zone97", 'TAA.csv', "zones")
 arcpy.CopyFeatures_management("temmp", "clustering.gdb/TAZ_join")
 TAZ_join='clustering.gdb/TAZ_join'     #filter_final_featureclass
 
 
 cluster='clustering.gdb/cluster'    #HH-HL-LL-LH
-arcpy.stats.ClustersOutliers(TAZ_join, 'TargetAudience_csv_TA', cluster, 'INVERSE_DISTANCE', 'EUCLIDEAN_DISTANCE', 'NONE')
+arcpy.stats.ClustersOutliers(TAZ_join, 'TAA_csv_TA', cluster, 'INVERSE_DISTANCE', 'EUCLIDEAN_DISTANCE', 'NONE')
 
 
 lyr="lyr"
@@ -64,21 +62,21 @@ cluster_HHHL="cluster_HHHL.shp"
 arcpy.CopyFeatures_management(lyr,cluster_HHHL) 
 arcpy.Delete_management(lyr)
 
-###################################################################     adddata_to-firstcluster(Moran)
-cluster_xls='cluster_HHHL.xls'
+# ###################################################################     adddata_to-firstcluster(Moran)
+cluster_xls='{0}/cluster_HHHL.xls'.format(dir)
 arcpy.conversion.TableToExcel(cluster_HHHL, cluster_xls)
 
-dfC = pd.read_excel('{0}/cluster_HHHL.xls'.format(dir))
+dfC = pd.read_excel(cluster_xls)
 dfTAZ=pd.read_csv('{0}/TAZ.csv'.format(dir))
 final=pd.merge(dfC,dfTAZ,left_on='SOURCE_ID',right_on='OBJECTID',how='outer')
-finall=final[['SOURCE_ID','TAZ_zone97','COType','TargetAudi']]
+finall=final[['SOURCE_ID','TAZ_zone97','COType','TAA_csv_TA']]
 # finall.to_csv('{0}/clusterT_{1}_{2}.csv'.format(dir,brand_name,date_time))              ******SS
 filt=finall['COType'].isin(['HH','HL'])
 finalll=finall[filt]
 finalll.to_csv('{0}/cluster_{1}_{2}.csv'.format(dir,brand_name,date_time))
 
-# os.remove('TargetAudience.csv')
-os.remove('cluster_HHHL.xls')
+os.remove('{0}/TAA.csv'.format(dir))
+os.remove('{0}/cluster_HHHL.xls'.format(dir))
 
 arcpy.env.workspace = "{0}/clustering.gdb".format(dir)
 arcpy.Delete_management('TAZ_join',"")
@@ -92,23 +90,23 @@ finalll_addcoloum=pd.merge(finalll,dff[TargetAgess],left_on='TAZ_zone97',right_o
 finalll_addcoloum.to_csv('{0}/cluster_data_{1}_{2}.csv'.format(dir,brand_name,date_time))
 
 #data_csv_final
-################################################################################################################
+# ################################################################################################################
 # arcpy.Delete_management('temmp')
 finalll_addcoloumn=finalll_addcoloum[TargetAgessss]
 
 # finalll_addcoloumn.columns = finalll_addcoloumn.columns.str.replace('SOURCE_ID', 'SOURCE_ID')
 
-finalll_addcoloumn.to_csv('{0}/join.csv'.format(dir,brand_name,date_time))
+finalll_addcoloumn.to_csv('{0}/join.csv'.format(dir))
 
 arcpy.MakeFeatureLayer_management("{0}/cluster_HHHL.shp".format(dir), "temmpp")   #11ta field dare
-arcpy.AddJoin_management("temmpp", "SOURCE_ID", '{0}/join.csv'.format(dir,brand_name,date_time), "SOURCE_ID")
-arcpy.CopyFeatures_management("temmpp", "{0}/cluster_data_{1}_{2}.shp".format(dir,brand_name,date_time))
+arcpy.AddJoin_management("temmpp", "SOURCE_ID", '{0}/join.csv'.format(dir), "SOURCE_ID")
+arcpy.CopyFeatures_management("temmpp", "{0}/cluster_data_{1}_{2}.shp".format(dir,brand_name,date_time))    #cluster_HHHL.shp + cluster_Data.csv (list of columns)
 arcpy.Delete_management('temmpp')
-os.remove('{0}/join.csv'.format(dir,brand_name,date_time))
-arcpy.Delete_management('cluster_HHHL.shp',"")
+os.remove('{0}/join.csv'.format(dir))
+arcpy.Delete_management('{0}/cluster_HHHL.shp'.format(dir),"")
 
-#################################################################################################################Grouping
-input_grouping='{0}/cluster_data_{1}_{2}.shp'.format(dir,brand_name,date_time)
+# #################################################################################################################Grouping
+input_grouping='{0}/cluster_data_{1}_{2}.shp'.format(dir,brand_name,date_time)    
 output_grouping='{0}/Grouping_data_{1}_{2}.shp'.format(dir,brand_name,date_time)
 Output_Report_File='{0}/Grouping_report_{1}_{2}.pdf'.format(dir,brand_name,date_time)
 
@@ -120,7 +118,7 @@ for field in fields:
 Analysis_Fields=field_names[ii:jj]
 
 arcpy.stats.GroupingAnalysis(input_grouping, "cluster_HH", output_grouping, "2", Analysis_Fields, 
-                             "NO_SPATIAL_CONSTRAINT", "EUCLIDEAN","","","FIND_SEED_LOCATIONS","",Output_Report_File,
+                             "NO_SPATIAL_CONSTRAINT", "EUCLIDEAN","","","FIND_SEED_LOCATIONS","","",
                              "DO_NOT_EVALUATE") 
 #finalCSV
 output_grouping_xls_file='{0}/Grouping_data_{1}_{2}.xls'.format(dir,brand_name,date_time)
@@ -136,11 +134,19 @@ arcpy.analysis.Statistics(output_grouping, output_grouping_table,[[statistics_fi
 out_xls_file='{0}/sumstats_{1}_{2}.xls'.format(dir,brand_name,date_time)
 arcpy.TableToExcel_conversion (output_grouping_table, out_xls_file)
 df=pd.read_excel (r'{0}/sumstats_{1}_{2}.xls'.format(dir,brand_name,date_time))
-print(df.iloc[0,3])
+
 if df.iloc[0,3] > df.iloc[1,3]:
     priority=1
 else :
       priority=2
 
 os.remove('{0}/sumstats_{1}_{2}.xls'.format(dir,brand_name,date_time))
-arcpy.Delete_management('sumstats_{0}_{1}.xls'.format(brand_name,date_time),"")
+
+
+
+def dfToJson(df):
+    print(df.to_json(orient='index'))
+
+print()
+
+dfToJson(final_grouping_clustering_df)
---
+++ destination/destination.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+import arcpy
+import timestamp
+from datetime import datetime
+import os
+
+#####################################################
+
+dir="D:/atrin/atrin/clustering"
+dirr="D:/atrin/atrin/clustering/destination"
+
+# inputs
+brand_name='BrandName'
+now = "now"   #time of first run
+cluster='{0}/cluster_data_BrandName_20220713_135523.csv'.format(dirr)          #cluster_data_{1}_{2}.csv'.format(dir,brand_name,now) 
+
+##############################################################################
+
+clusterDF = pd.read_csv (cluster)
+OD= pd.read_csv (r'{0}/OD.csv'.format(dirr))
+
+clusterDF.insert(1,"TAPercent",clusterDF['TAA_csv_TA']/clusterDF['POP']) 
+clusterDFF = clusterDF.sort_values('TAZ_zone97')
+AllZoneList= clusterDFF['TAZ_zone97']    #Origin zones of TA
+clusterDFF=clusterDFF.reset_index()  
+# print(clusterDFF)
+filt = OD['zones'].isin(AllZoneList) 
+ODD=OD[filt]
+Des=ODD.reset_index()  
+# print(Des)      
+DesTA=Des.iloc[:,2:].multiply(clusterDFF['TAPercent'].squeeze(),axis=0)         #OD of orgin zones of TA
+# print(DesTA)
+DesTAAgg=DesTA.sum(axis=0)       #aggregated destination of all clustered TA    #just for graphs
+
+#######################################################################################################
+DesTA.insert(0,"TA",clusterDFF['TAA_csv_TA'])
+DesTA.insert(0,"Regions",clusterDFF['Regions'])
+DesTA.insert(0,"zonesO",Des['zones'])
+DesTAGroup=DesTA.groupby(['Regions']).sum()               #Aggregated Destination of all Grouping clustered TA  
+# print (DesTAGroup)
+##############################################################################################
+
+temp = np.flip(DesTAGroup.iloc[:,2:].values.argsort(),1)
+DesTAGroupSortLable = pd.DataFrame(DesTAGroup.iloc[:,2:].columns[temp])       #find the column names in descending order for each row
+DesTAGroupSortLable.insert(0,"Regions",DesTAGroup.index)
+
+# print(DesTAGroupSortLable)
+   
+DesTAGroupSortValue=pd.DataFrame(  np.sort(DesTAGroup.iloc[:,2:].values, axis=1)[:,::-1])       # values in descending order for each row
+
+# print(DesTAGroupSortValue)    
+
+DesTAGroupSortValueCum=DesTAGroupSortValue.cumsum(axis=1)                                 # cumulative values which were in descending order for each row
+DesTAGroupSortValueCum.insert(0,"Regions",DesTAGroup.index) 
+# print(DesTAGroupSortValueCum) 
+DesTAGroup=DesTAGroup.reset_index()
+DesTAGroupSortValueCum.insert(0,"TA",DesTAGroup['TA'])
+print(DesTAGroupSortValueCum)
+# #######################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# i= (DesTAGroup['0'] + DesTAGroup['2']).to_list()
+# print (i)     
+# arcpy.env.workspace = "{0}".format(dir)
+# Regions='clustering.gdb/RegionTeh'.format(dir)    
